7d98be04e742b0a294e5c186b099eaa48ecd8c5d
Gabriele Bologna <bolognagabriele2003@gmail.com>
2023-08-10 20:03:29 +0200

    First commit

---
+++ Splitter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace LinesSplitter
+{
+    public class Splitter
+    {
+        public static List<string> Split(string input)
+        {
+            List<string> splitted = new List<string>();
+
+            foreach (string line in SplitToLines(input))
+            {
+                splitted.Add(line);
+            }
+
+            return splitted;
+        }
+
+        private static IEnumerable<string> SplitToLines(string input)
+        {
+            if (input == null)
+            {
+                yield break;
+            }
+
+            using (System.IO.StringReader reader = new System.IO.StringReader(input))
+            {
+                string line;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    yield return line;
+                }
+            }
+        }
+    }
+}
